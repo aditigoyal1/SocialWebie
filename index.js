a6321c711bfbf76c925a4ce980e56664284fc09c
aditigoyal1 <aditiprotocol1@gmail.com>
2020-03-26 18:07:13 +0530

    set up view engine and views

--- index.js
+++ index.js
@@ -5,6 +5,12 @@
    //use routes
 
    app.use('/',require('./routes'));
+
+
+
+   // setting ejs
+   app.set('view engine','ejs');
+   app.set('views','./views');
    
    app.listen(port,function(err){
        if(err)
