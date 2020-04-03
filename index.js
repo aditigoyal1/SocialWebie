93cb83ce1d4b2e0f5601e49603d8c7a2f4c40545
aditigoyal1 <aditiprotocol1@gmail.com>
2020-04-03 14:01:09 +0530

    add sass and styles to header and footer

--- index.js
+++ index.js
@@ -10,6 +10,18 @@
    const expressLayouts=require('express-ejs-layouts');
    const cookieParser=require('cookie-parser');
 
+   const sassMiddleware=require('node-sass-middleware');
+
+   app.use(sassMiddleware({
+     src:'./assets/scss',
+     dest:'./assets/css',
+     debug:true,
+     outputStyle:'extended',
+     prefix:'/css'
+   }));
+
+
+
 
    app.use(express.urlencoded());
    app.use(cookieParser());
