ff025191962e0dfb8750e98430674347b356d02e
aditigoyal1 <aditiprotocol1@gmail.com>
2020-04-08 15:01:08 +0530

    Delete the comment

--- routes/comments.js
+++ routes/comments.js
@@ -6,5 +6,6 @@ const passport=require('../config/passport-local-strategy');
 const commentsController=require('../controllers/comment_controller');
 
 router.post('/create',passport.checkAuthentication,commentsController.create);
+router.get('/destroy/:id',passport.checkAuthentication,commentsController.destroy);
 
 module.exports=router;
