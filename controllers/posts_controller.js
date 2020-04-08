3fee472581749c26176cb566dca76bacfaa2c48d
aditigoyal1 <aditiprotocol1@gmail.com>
2020-04-08 13:52:14 +0530

    delete the post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
-const Post=require('../models/post')
+const Post=require('../models/post');
+const Comment=require('../models/comment');
 module.exports.create=function(req,res)
 {
 Post.create({
@@ -13,4 +14,26 @@ Post.create({
         return res.redirect('back');
     });
 }
+module.exports.destroy=function(req,res)
+{
+    Post.findById(req.params.id,function(err,post){
+        if(err)
+        {
+            console.log("Error in finding the post while deleting");
+            return;
+        }
 
+        if(post.user==req.user.id)
+        {
+            post.remove();
+            Comment.deleteMany({post:req.params.id},function(err){
+                return res.redirect('back');
+            });
+
+        }
+        else{
+            return res.redirect('back');
+        }
+
+    });
+}
--- routes/post.js
+++ routes/post.js
@@ -6,5 +6,7 @@ const passport=require('../config/passport-local-strategy');
 const postsController=require('../controllers/posts_controller');
 
 router.post('/create',passport.checkAuthentication,postsController.create);
+router.get('/destroy/:id',passport.checkAuthentication,postsController.destroy);
+
 
 module.exports=router;
