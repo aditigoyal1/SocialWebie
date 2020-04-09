73ec101fb0059487afa88a884780547e1c0072b5
aditigoyal1 <aditiprotocol1@gmail.com>
2020-04-09 12:57:40 +0530

    provide list of all user's profile link

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,9 +3,14 @@ const User=require("../models/user");
 
 module.exports.profile=function(req,res)
 {
-    return res.render('user_profile',{
-        title:"user profile",
+    User.findById(req.params.id,function(err,user){
+        return res.render('user_profile',{
+            title:"user profile",
+            profile_user:user
+        })
+
     })
+    
 }
 
 module.exports.signUp=function(req,res)
--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const usersController=require('../controllers/users_controller');
 
 const passport=require('passport');
 
-router.get('/profile',passport.checkAuthentication,usersController.profile);
+router.get('/profile/:id',passport.checkAuthentication,usersController.profile);
 
 
 router.get('/sign-up',usersController.signUp);
