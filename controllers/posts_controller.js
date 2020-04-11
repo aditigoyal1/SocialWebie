3c96e8444cd963ddd0fb28c4cc1f8b3aa627abed
aditigoyal1 <aditiprotocol1@gmail.com>
2020-04-11 13:56:56 +0530

    create a post:send post usng Ajax

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -6,6 +6,15 @@ try {
         let post= await Post.create({
         content:req.body.content,
         user:req.user._id});
+
+        if(req.xhr){
+            return res.status(200).json({
+                data:{
+                    post:post
+                },
+                message:"Post created!",
+            })
+        }
         
         req.flash('success',"Post Published!");
         return res.redirect('back');
