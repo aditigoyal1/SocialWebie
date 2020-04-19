55f4310f218b10a9cb87edd58215efcbb9b30da9
aditigoyal1 <aditiprotocol1@gmail.com>
2020-04-19 13:47:52 +0530

    set up nodemailer

---
+++ config/nodemailer.js
@@ -0,0 +1,43 @@
+const nodemailer=require('nodemailer');
+const ejs=require('ejs');
+const path=require('path');
+
+
+
+let transporter=nodemailer.createTransport({
+    service:'gmail',
+    host:'smtp.gmail.com',
+    port:587,
+    secure:false,
+    auth:{
+        user:'aditiprotocol1@gmail.com',
+        pass:12345
+    }
+});
+
+
+let renderTemplate=(data,relativepath)=>{
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname,"../views/mailers",relativepath),
+        data,
+        function(err,template){
+
+            if(err)
+            {
+                console.log("Error in Rendering the template");
+                return;
+            }
+
+        mailHTML=template;
+        });
+
+        return mailHTML; 
+
+    
+}
+
+module.exports={
+    transporter:transporter,
+    renderTemplate:renderTemplate
+}
